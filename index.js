bacfeab20aa5831a375e56c1448708431278e174
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-06-23 23:48:08 +0100

    Done with user object

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+
+// Javscript Variables
 const app = express()
 const PORT = 5000
 const mongoClientUrl = "mongodb://127.0.0.1:27017/primegaininvestments"
@@ -10,9 +12,19 @@ app.use(express.json())
 app.use(cors({ origin: "*" }))
 
 // Initialize Route
+const signup = require("./routes/signup")
+const signin = require('./routes/signin')
+const dashboard = require("./routes/dashboard")
+const adminDashboard = require('./routes/admindashboard')
 
 //Assign Route
+app.use("/signup", signup);
+app.use("/signin", signin);
+app.use("/dashboard", dashboard);
+app.use("/admin", adminDashboard);
 
+
+// Report Logs
 app.listen(PORT, () => {
     console.log("Server is live on: " + PORT);
     mongoose.connect(mongoClientUrl)
--- models/User.js
+++ models/User.js
@@ -1,7 +1,37 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 
 // username, email, telephone, dateAccountCreated, password
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        max: 256
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 256
+    },
+    telephone: {
+        type: Number,
+        required: true,
+        max: 20
+    },
+    dateAccountCreated: {
+        type: String,
+        max: 256
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 256
+    },
+})
 
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
 })
 module.exports = mongoose.Model("User", userSchema)
