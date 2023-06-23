c803fd70ac359ecef888cd30d21443d5c32b116c
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-06-24 00:32:29 +0100

    Signup route done not tested

--- routes/signup.js
+++ routes/signup.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router()
 const mongoose = require('mongoose')
+const User = require('../models/User')
 
 
-router.get("/", (req, res) => {
-
-})
+// username, email, telephone, dateAccountCreated, password
 router.post('/', (req, res) => {
+    var username = req.body.username
+    var email = req.body.email
+    var telephone = req.body.telephone
+    var dateAccountCreated = req.body.dateAccountCreated
+    var password = req.body.password
 
+    const user = new User({ username: username, email: email, telephone: telephone, dateAccountCreated: dateAccountCreated, password: password })
+    User.findOne({ email, username })
+    .then((response) => {
+        if (response == null) {
+            user.save()
+                .then(res.status(200).send())
+                .catch(err => res.status(501).send())
+        }
+        else {
+            res.status(401).send()
+        }
+    })
+    .catch((err) => { console.log(err), res.send(501) })
 })
 
 module.exports = router
